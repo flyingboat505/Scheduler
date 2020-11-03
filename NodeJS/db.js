2bebefdc7682f444563b6d0d9b7ee93765dde5dc
flyingboat505 <abhilashmunnangi@gmail.com>
2020-11-03 13:01:00 -0800

    DB Connection

--- NodeJS/db.js
+++ NodeJS/db.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost:27017/CrudDB', (err) => {
+const URL = 'mongodb+srv://AbhiM:1234@cluster0.s0kwt.mongodb.net/<dbname>?retryWrites=true&w=majority';
+mongoose.connect(URL, (err) => {
     if (!err)
-        console.log('MongoDB connection succeeded.');
+        console.log('MongoDB connection succeeded..');
     else
         console.log('Error in DB connection : ' + JSON.stringify(err, undefined, 2));
 });
